ec58198d84b3d8392ef152b1987861561036a1f8
Derbes Utebaliyev <uderbes@gmail.com>
2025-11-17 18:24:11 +0500

    add client, cancel, pgx, remove providers, pg, fix comment for issue-1

--- client/weather_client.go
+++ client/weather_client.go
@@ -1,4 +1,4 @@
-package internal
+package client
 
 import (
 	"context"
@@ -9,61 +9,64 @@ import (
 	"time"
 )
 
-func FeatchOpenWeather(ctx context.Context, client *http.Client, city string) (*models.OpenWeather, error) {
-	if client == nil {
-		client = &http.Client{Timeout: 5 * time.Second}
+type Client struct {
+	cli               *http.Client
+	openWeatherApiKey string
+	weatherApiKey     string
+}
+
+func NewClient(openWeatherApiKey, weatherApiKey string) *Client {
+	return &Client{
+		cli: &http.Client{
+			Timeout: 5 * time.Second,
+		},
+		openWeatherApiKey: openWeatherApiKey,
+		weatherApiKey:     weatherApiKey,
 	}
-	apiKeyOpenWeather := GetEnv("API_KEY_OPEN_WEATHER", "")
-	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, apiKeyOpenWeather)
+}
+
+func (c *Client) FetchOpenWeather(ctx context.Context, city string) (*models.OpenWeather, error) {
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric",
+		city, c.openWeatherApiKey)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("build request: %w", err)
 	}
 
-	resp, err := client.Do(req)
+	resp, err := c.cli.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("request weather for %s: %w", city, err)
 	}
-
 	defer resp.Body.Close()
 
 	var data models.OpenWeather
-
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		fmt.Printf("Error decoding whether data for %s, with error %w", city, err)
 		return nil, fmt.Errorf("decode weather response for %s: %w", city, err)
 	}
 
 	return &data, nil
 }
 
-func FetchWeatherApi(ctx context.Context, client *http.Client, city string) (*models.WeatherApi, error) {
-	if client == nil {
-		client = &http.Client{Timeout: 5 * time.Second}
-	}
-	apiKeyWeatherApi := GetEnv("API_KEY_WEATHER_API", "")
-	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?q=%s&key=%s", city, apiKeyWeatherApi)
+func (c *Client) FetchWeatherApi(ctx context.Context, city string) (*models.WeatherApi, error) {
+	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?q=%s&key=%s",
+		city, c.weatherApiKey)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("build request: %w", err)
 	}
 
-	resp, err := client.Do(req)
+	resp, err := c.cli.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("request weather for %s: %w", city, err)
 	}
-
 	defer resp.Body.Close()
 
 	var data models.WeatherApi
-
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		fmt.Printf("Error decoding whether data for %s, with error %w", city, err)
 		return nil, fmt.Errorf("decode weather response for %s: %w", city, err)
 	}
 
 	return &data, nil
-
 }
--- services/services.go
+++ services/services.go
@@ -3,36 +3,50 @@ package services
 import (
 	"context"
 	"fmt"
-	"go-first-project/internal"
+	"go-first-project/client"
 	"go-first-project/models"
 	"go-first-project/repository"
-	"net/http"
 	"time"
 )
 
 type WeatcherService struct {
-	repo *repository.WeatherRepository
+	repo   *repository.WeatherRepository
+	client *client.Client
 }
 
-func NewWeatcherService(repo *repository.WeatherRepository) *WeatcherService {
-	return &WeatcherService{repo: repo}
+func NewWeatcherService(repo *repository.WeatherRepository, client *client.Client) *WeatcherService {
+	return &WeatcherService{
+		repo:   repo,
+		client: client,
+	}
 }
 
-func (s *WeatcherService) GetWeather(ctx context.Context, client *http.Client, city string) (*models.WeatherApiResponse, error) {
+func (s *WeatcherService) GetWeather(ctx context.Context, city string) (*models.WeatherApiResponse, error) {
 	start := time.Now()
+
+	apiCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
 
 	openWeatherChan := make(chan *models.OpenWeather)
 	weatherApiChan := make(chan *models.WeatherApi)
 
 	go func() {
-		w, _ := internal.FeatchOpenWeather(ctx, client, city)
-		fmt.Println("OpenWeather time:", time.Since(start))
+		w, err := s.client.FetchOpenWeather(apiCtx, city)
+		if err != nil {
+			fmt.Printf("Error fetching OpenWeather: %v\n", err)
+			return
+		}
+		fmt.Printf("OpenWeather time: %v\n", time.Since(start))
 		openWeatherChan <- w
 	}()
 
 	go func() {
-		w2, _ := internal.FetchWeatherApi(ctx, client, city)
-		fmt.Println("WeatherApi time:", time.Since(start))
+		w2, err := s.client.FetchWeatherApi(apiCtx, city)
+		if err != nil {
+			fmt.Printf("Error fetching WeatherApi: %v\n", err)
+			return
+		}
+		fmt.Printf("WeatherApi time: %v\n", time.Since(start))
 		weatherApiChan <- w2
 	}()
 
@@ -40,6 +54,7 @@ func (s *WeatcherService) GetWeather(ctx context.Context, client *http.Client, c
 
 	select {
 	case w := <-openWeatherChan:
+		cancel()
 		response.Temp = w.Main.Temp
 		response.FeelsLike = w.Main.FeelsLike
 		response.Wind = w.Wind.Speed
@@ -47,24 +62,25 @@ func (s *WeatcherService) GetWeather(ctx context.Context, client *http.Client, c
 		response.Humidity = w.Main.Humidity
 		response.Provider = "OpenWeather"
 	case w2 := <-weatherApiChan:
+		cancel()
 		response.Temp = w2.Current.Temp
 		response.FeelsLike = w2.Current.FeelsLike
 		response.Wind = w2.Current.Wind
 		response.City = w2.Location.Name
 		response.Humidity = w2.Current.Humidity
 		response.Provider = "WeatherApi"
-	case <-time.After(10 * time.Second):
-		fmt.Println("Timeout - no response received")
+	case <-apiCtx.Done():
+		fmt.Printf("Timeout - no response received")
 		return nil, fmt.Errorf("timeout - no response received")
 	}
 
 	weatherData := &models.WeatherData{
-		City:         response.City,
-		Temp: response.Temp,
-		Humidity:     response.Humidity,
-		Wind:    response.Wind,
+		City:      response.City,
+		Temp:      response.Temp,
+		Humidity:  response.Humidity,
+		Wind:      response.Wind,
 		FeelsLike: response.FeelsLike,
-		Provider:     response.Provider,
+		Provider:  response.Provider,
 	}
 
 	err := s.repo.CreateWeather(ctx, weatherData)
